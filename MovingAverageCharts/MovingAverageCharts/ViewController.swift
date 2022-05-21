434dfbb15658e0462dc3bc5db882c51150acc3b4
Zoe Liauh <woanjwuliauh@gmail.com>
2022-05-21 20:50:45 +0800

    setup lineChartView with mock data

--- MovingAverageCharts/MovingAverageCharts/ViewController.swift
+++ MovingAverageCharts/MovingAverageCharts/ViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
     
     var tsmcModel: TSMCModel?
     let jsonParseHelper = JSONParseHelper()
+    var entries: [ChartDataEntry]?
     
     @IBOutlet private weak var dataView: DateView!
     @IBOutlet private weak var stockPriceView: PERatioPriceView!
@@ -20,6 +22,7 @@ class ViewController: UIViewController {
     @IBOutlet private weak var peRatioPriceView4: PERatioPriceView!
     @IBOutlet private weak var peRatioPriceView5: PERatioPriceView!
     @IBOutlet private weak var peRatioPriceView6: PERatioPriceView!
+    @IBOutlet private weak var movingAverageChartView: LineChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,5 +36,39 @@ class ViewController: UIViewController {
         self.peRatioPriceView5.configureView(color: .purple, labelTitle: "10.4倍145.6")
         self.peRatioPriceView6.configureView(color: .black, labelTitle: "9.7倍43.4")
         tsmcModel = jsonParseHelper.parseJson(form: JSONFileName.TSMCMovingAverage.rawValue)
+        setupChart()
+    }
+    
+    // 建立 ChartDataEntry
+    func setupChart() {
+        movingAverageChartView.noDataText = "wait loading..."
+        movingAverageChartView.xAxis.labelPosition = .bottom
+        movingAverageChartView.leftAxis.enabled = false
+        movingAverageChartView.xAxis.drawAxisLineEnabled = false
+        movingAverageChartView.legend.enabled = false
+        movingAverageChartView.rightAxis.forceLabelsEnabled = true
+        movingAverageChartView.drawMarkers = true
+        let points = [30.65, 44.56, 25.64, 34.66, 25.20, 30.65, 44.56, 25.64, 34.66, 25.20, 54.32, 51.23]
+        entries = points.enumerated().map {
+            return ChartDataEntry.init(x: Double($0), y: Double($1))
+        }
+        let set = LineChartDataSet.init(entries: entries)
+        set.drawCirclesEnabled = false
+        set.drawValuesEnabled = false
+        set.lineWidth = 2.0
+        set.setColor(.red)
+        
+        let data = LineChartData(dataSets: [set])
+        movingAverageChartView.data = data
+    }
+    
+    func setupXAxis() {
+        var xValue: [String] = []
+//        for i in 0..<entries?.count {
+//            let data = Calendar.current.date(byAdding: .day, value: i, to: Date()) ?? Date
+//            xValue.append("2/11")
+//        }
+        movingAverageChartView.xAxis.labelPosition = .bottom
+        movingAverageChartView.leftAxis.enabled = false
     }
 }
